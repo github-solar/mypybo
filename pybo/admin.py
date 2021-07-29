9562e6545b761f60f33d672b56dc850dd1bb343f
github-solar <solarsun@nate.com>
2021-07-29 23:48:34 +0900

    mypybo 배포후 admin.py 오류수정

--- pybo/admin.py
+++ pybo/admin.py
@@ -1,8 +1,9 @@
 
 from django.contrib import admin
-from .models import Question, Answer
+from .models import Question, Answer, Comment
 
 admin.site.register(Question)
 admin.site.register(Answer)
+admin.site.register(Comment)
 
 # Register your models here.
